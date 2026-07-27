38fd968b9c1a8238cb74bd3157c9dc1ea2bb28a4
koro <11595941@qq.com>
2026-07-27 22:15:59 +0800

    fix bug

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,4 +1,4 @@
-import redis from '../../../lib/redis';
+import redis from '../../lib/redis';
 
 export default async function handler(req, res) {
   if (req.headers['x-password'] !== process.env.ADMIN_PASSWORD) {
